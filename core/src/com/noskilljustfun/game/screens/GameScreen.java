b30557515de27c873d73a6d01eab6cc56d2add01
Kacper Wysocki <kacperkapiwysocki@gmail.com>
2017-04-23 14:15:57 +0200

    create lvl 1 map

--- core/src/com/noskilljustfun/game/screens/GameScreen.java
+++ core/src/com/noskilljustfun/game/screens/GameScreen.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 public class GameScreen extends BaseScreen {
 
-    private static final int BLOCKS_AMOUNT=11;
+    public static final int BLOCKS_AMOUNT = 11;
     public static final int BULLETS_COUNT = 20;
     public static int bulletCounter = 0;
 
@@ -38,11 +38,13 @@ public class GameScreen extends BaseScreen {
         super(game);
 
         player = new TankPlayer();
-        enemy1 = new TankEnemy();
-        enemy2 = new TankEnemy(Gdx.graphics.getWidth()/4, Gdx.graphics.getHeight()/4);
-        enemy3 = new TankEnemy(Gdx.graphics.getWidth()/3,Gdx.graphics.getHeight()/3);
-        initGameBlocks();
+        enemy1 = new TankEnemy(850,900);
+        enemy2 = new TankEnemy(410, 900);
+        enemy3 = new TankEnemy(1290,900);
+      // initGameBlocks();
         initBullets();
+        LevelGenerator.getInstance().setStage(stage);
+        LevelGenerator.getInstance().initLevelOne();
         EnvironmentCollisionManager.getInstance().setStage(stage);
         stage.addActor(player);
         stage.addActor(enemy1);
@@ -69,9 +71,9 @@ public class GameScreen extends BaseScreen {
         enemy1.update(delta);
         enemy2.update(delta);
         enemy3.update(delta);
-        enemy1.enemyShoot(delta,this.stage,this.bullets);
+       /* enemy1.enemyShoot(delta,this.stage,this.bullets);
         enemy2.enemyShoot(delta,this.stage,this.bullets);
-        enemy3.enemyShoot(delta,this.stage,this.bullets);
+        enemy3.enemyShoot(delta,this.stage,this.bullets);*/
         updateBullets();
         stage.draw();
         spriteBatch.end();
