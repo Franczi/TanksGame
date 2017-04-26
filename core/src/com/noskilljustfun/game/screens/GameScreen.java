13f30a239f0ec805b0b522c0ba0ee1a119ad39e1
Franczi <bartoszwieczorek1@gmail.com>
2017-04-26 22:06:10 +0200

    added background

--- core/src/com/noskilljustfun/game/screens/GameScreen.java
+++ core/src/com/noskilljustfun/game/screens/GameScreen.java
@@ -1,6 +1,8 @@
 package com.noskilljustfun.game.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.noskilljustfun.game.TanksGame;
@@ -31,6 +33,7 @@ public class GameScreen extends BaseScreen {
     private List<Bullet> bullets;
     private Label scoreLabel;
     private Label playerLifeLabel;
+    private Texture background;
 
     private Bullet bullet;
     private float shotTime;
@@ -44,6 +47,7 @@ public class GameScreen extends BaseScreen {
         enemy1 = new TankEnemy(850,900);
         enemy2 = new TankEnemy(410, 900);
         enemy3 = new TankEnemy(1290,900);
+        background = new Texture("background.png");
         initBullets();
         initPlayerLifeLabel();
         initScoreLabel();
@@ -69,11 +73,17 @@ public class GameScreen extends BaseScreen {
     public void render(float delta) {
 
         super.render(delta);
+
+        spriteBatch.begin();
+        stage.getBatch().begin();
+        stage.getBatch().draw(background, 370, 0, (float) 1100, (float) Gdx.graphics.getHeight());
+        stage.getBatch().end();
+
         stage.act();
         handleInput(delta);
         updateScoreLabel();
         updatePlayerLifeLabel();
-        spriteBatch.begin();
+
         player.update();
         enemy1.update(delta);
         enemy2.update(delta);
@@ -85,6 +95,8 @@ public class GameScreen extends BaseScreen {
         updateBullets();
         stage.draw();
         spriteBatch.end();
+
+
         controller.draw();
     }
 
